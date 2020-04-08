a3fe0d68cf30e3af9a7607eba793c5a8d86019c7
song <463020345@qq.com>
2020-04-08 20:25:48 +0800

    [ADD] 新增a.py

---
+++ a.py
@@ -0,0 +1,23 @@
+from flask import Flask, escape, url_for
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!123'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % escape(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
